7da1bb9652625cf62c03b04a7152c5dea544a1ea
Chahyo Purnomo Aji <ajizblast@gmail.com>
2023-11-08 21:36:35 +0700

    open connection in golang

--- Section 7 - Golang Database/database_test.go
+++ Section 7 - Golang Database/database_test.go
@@ -1,10 +1,19 @@
 package Section_7___Golang_Database
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"testing"
 )
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:tcp(localhost:3306)/")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
