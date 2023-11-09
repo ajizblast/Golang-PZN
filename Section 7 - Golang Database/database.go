f09417e2eb3aebec1cdabbebcae6bdaa1d1ad472
Chahyo Purnomo Aji <ajizblast@gmail.com>
2023-11-09 09:07:06 +0700

    add database pooling

---
+++ Section 7 - Golang Database/database.go
@@ -0,0 +1,18 @@
+package Section_7___Golang_Database
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:tcp(localhost:3306)/")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+}
