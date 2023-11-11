5ee56537238a32983271b1631f5f8005283e3b90
Chahyo Purnomo Aji <ajizblast@gmail.com>
2023-11-11 20:59:00 +0700

    Golang Web add Server

---
+++ Section 9 - Belajar Golang Web/server_test.go
@@ -0,0 +1,17 @@
+package Section_9___Belajar_Golang_Web
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
